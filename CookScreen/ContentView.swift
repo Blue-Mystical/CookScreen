794571ac1de042681f95997685a7629a5a1cb865
Blue-Mystical <magicalharuki@gmail.com>
2022-04-19 02:38:23 -0700

    Initial Commit

---
+++ CookScreen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CookScreen
+//
+//  Created by Blue Snow on 19/4/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CookScreen/CookScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookScreenApp.swift
+//  CookScreen
+//
+//  Created by Blue Snow on 19/4/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct CookScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
